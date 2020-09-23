23cbf0d7edaf28bfe1c80bb2304571d546ab2486
cylindricalcube@protonmail.com <cylindricalcube>
2020-09-23 01:54:46 -0400

    Working DB connection and migrations

--- src/config.py
+++ src/config.py
@@ -2,6 +2,12 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-s3cure-pla!nt3xt-s3cret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('PG_DATABASE_URL') or 'localhost'
+    DB_USER = os.environ.get('PG_DB_USER') or 'todoozle'
+    DB_PASS = os.environ.get('PG_DB_PASS') or 'toor'
+    DB_HOST = os.environ.get('PG_DB_HOST') or 'localhost'
+    DB_PORT = os.environ.get('PG_DB_PORT') or '5432'
+    DB_NAME = os.environ.get('PG_DB_NAME') or 'todoozle'
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(
+        DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # DATABASE_PORT = os.environ.get('PG_DATABASE_PORT') or '5432'
