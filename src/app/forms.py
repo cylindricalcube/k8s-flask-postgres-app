45c4ba826abe919a60f9801adb644645c006f1fc
cylindricalcube@protonmail.com <cylindricalcube>
2020-09-23 02:49:09 -0400

    Simplify app to meet requirements and not complicate

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class NewListForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+class NewItemForm(FlaskForm):
+    task = StringField('Item', validators=[DataRequired()])
     submit = SubmitField('Create')
--- src/app/routes.py
+++ src/app/routes.py
@@ -1,20 +1,23 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import NewListForm
+from app import app, db
+from app.forms import NewItemForm
+from app.models import TodoItem
 
 @app.route('/')
 @app.route('/index')
 def index():
-    lists = [
-        { 'id': 1, 'name': 'Shopping' },
-        { 'id': 2, 'name': 'Groceries' }
-    ]
-    return render_template('index.html', lists=lists)
+    items = TodoItem.query.all()
+    return render_template('index.html', items=items)
 
-@app.route('/new_list', methods=['GET', 'POST'])
-def new_list():
-    form = NewListForm()
+@app.route('/new_item', methods=['GET', 'POST'])
+def new_item():
+    form = NewItemForm()
     if form.validate_on_submit():
-        flash('Created list {}'.format(form.name.data))
+        flash('Created item {}'.format(form.task.data))
+        # TODO: sanitize
+        ti = TodoItem(task=form.task.data)
+        # TODO: Add error handling
+        db.session.add(ti)
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('new_list.html', title='New List', form=form)
+    return render_template('new_item.html', title='New Item', form=form)
